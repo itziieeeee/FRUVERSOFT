3ab10e4826459e905095c7499daf4f089ebed245
Hannie Itzel <hitzel.b.g@gmail.com>
2026-04-14 13:22:43 -0600

    "cabio databse"

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'Fruver_',
+        'database'     => 'fruver',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
