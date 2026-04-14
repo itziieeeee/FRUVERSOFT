cc0db0e0429b6c61d94e847582ce62407b3151e8
_edu.gar <jgarciaed535@gmail.com>
2026-04-14 00:57:21 -0600

    Se corrigieron las rutas relativas, estamos en FRUVERSOFT no FRUVERSOFT2.0, chequen eso :v

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
- public string $baseURL = 'http://localhost/FRUVERSOFT2.0/public/';
+ public string $baseURL = 'http://localhost/FRUVERSOFT/public/';
   
 
     /**
